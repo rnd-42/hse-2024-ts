aadaeb45003e37774c6321a6acc5d6418138efd1
Anastasia <156711668+kimlaka@users.noreply.github.com>
2025-04-01 13:37:43 +0300

    fix: added constraints to WHERE clause in SQL query

--- calculating_elasticity/db_interaction.py
+++ calculating_elasticity/db_interaction.py
@@ -36,7 +36,7 @@ def read_from_postgres(engine=None):
     if engine is None:
         engine = get_engine()
     query = """
-    select val as trips, att_val as attribute, t5.dt as date, t5.product_id, t5.attribute_id
+    select val as trips, att_val as attribute, t5.dt as date, t5.product_id, t6.attribute_id
     from (select sum(t1.value::float) as val,
         start_dt as dt, t4.product_id, t1.attribute_id
     from ts_schema.timestamps t1
@@ -46,7 +46,8 @@ def read_from_postgres(engine=None):
     on t1.time_series_id = t3.time_series_id
     join ts_schema.products t4
     on t3.product_id = t4.product_id
-    group by dt, t4.product_id, t1.time_series_id, t1.attribute_id) t5
+    and t2.name = 'traffic'
+    group by dt, t4.product_id, t1.attribute_id) t5
     join (select avg(t1.value::float) as att_val,
         start_dt as dt, t4.product_id, t1.attribute_id
     from ts_schema.timestamps t1
@@ -56,10 +57,10 @@ def read_from_postgres(engine=None):
     on t1.time_series_id = t3.time_series_id
     join ts_schema.products t4
     on t3.product_id = t4.product_id
+    and t2.name not in ('traffic')
     group by dt, t4.product_id, t1.attribute_id) t6
     on t5.dt = t6.dt
     and t5.product_id = t6.product_id
-    and t5.attribute_id = t6.attribute_id
     order by date
     """
     df = pd.read_sql(query, engine)
